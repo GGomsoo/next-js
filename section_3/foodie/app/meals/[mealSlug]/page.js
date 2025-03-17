89b0647712e60968d4700f212b0c2aa237bc08ec
Park Jong Hyuk <occultism22@naver.com>
2025-03-17 12:58:35 +0900

    98. 연습문제 라우터 설정 동적 라우터 설정

---
+++ section_3/foodie/app/meals/[mealSlug]/page.js
@@ -0,0 +1,10 @@
+const MealDetailsPage = ({ params }) => {
+  return (
+    <>
+      <h1>Meal Details Page</h1>
+      <p>{params.mealSlug}</p>
+    </>
+  );
+};
+
+export default MealDetailsPage;
