e16ed9039a20273cfda8445224c9b04d318d304e
Park Jong Hyuk <occultism22@naver.com>
2025-03-17 14:56:42 +0900

    110. NextJS 및 풀스택 기능을 활용한 데이터 불러오기 컴포넌트에 async/await 적용. 서버 컴포넌트라 가능한 일

---
+++ section_3/foodie/lib/meals.js
@@ -0,0 +1,17 @@
+// sql import
+import sql from "better-sqlite3";
+import { resolve } from "styled-jsx/css";
+
+// DB의 이름을 문자열로 sql 함수에 전달
+const db = sql("meals.db");
+
+export const getMeals = async () => {
+  // 임의로 추가 지연 생성
+  // State(상태) 로딩 다룰 때 도움
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // all: 데이터를 불러올 때
+  // run: 데이터를 주입시킬 때
+  // get: 한가지 열만 찾고싶을 때
+  return db.prepare("SELECT * FROM meals").all();
+}
