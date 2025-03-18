1a9b9a187bfe5a4ce7bf083633866c62dcde6dbe
Park Jong Hyuk <occultism22@naver.com>
2025-03-18 17:51:55 +0900

    커스텀 이미지 피커(선택 도구) 입력 컴포넌트에 대한 기초 useRef 훅, current로 접근하여 click method 사용

---
+++ section_3/foodie/components/meals/image-picker.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRef } from "react";
+import classes from "./image-picker.module.css"
+
+const ImagePicker = ({ label, name }) => {
+  const ImageInputRef = useRef();
+  const handlePickClick = () => {
+    ImageInputRef.current.click();
+  };
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={ImageInputRef}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}>
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ImagePicker;
