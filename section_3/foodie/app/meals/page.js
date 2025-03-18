558920cb04bfd97d61cbdbaca69270e3d4c10e15
Park Jong Hyuk <occultism22@naver.com>
2025-03-18 16:57:42 +0900

    112. Suspense & Streamed Response를 이용한 세분화 로딩 상태 관리 Suspence 컴포넌트를 사용 fallback에 로딩 상태에 보여줄 내용을 설정 해당 컴포넌트로 감싼 부분에서만 적용

--- section_3/foodie/app/meals/page.js
+++ section_3/foodie/app/meals/page.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css"
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-const MealsPage = async () => {
-  const meals =  await getMeals();
+const Meals = async () => {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +27,10 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        {/* Suspence: 데이터 불러올 때 까지 로딩 상태 처리하고 대체 컨텐츠를 표시할 수 있도록 도와줌*/}
+        <Suspense fallback={<p className={classes.loading}>fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
