0a03b1a7a85b40d2d9dbc6f0fd3850bf519f6b09
Park Jong Hyuk <occultism22@naver.com>
2025-03-18 17:59:32 +0900

    119. 피커에 이미지 미리보기 추가 FileReader 함수 활용

--- section_3/foodie/components/meals/image-picker.js
+++ section_3/foodie/components/meals/image-picker.js
@@ -1,18 +1,42 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css"
+import Image from "next/image";
 
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, seetPickedImage] = useState();
   const ImageInputRef = useRef();
+
   const handlePickClick = () => {
     ImageInputRef.current.click();
+  };
+
+  const handleImageChange = (e) => {
+    // 선택한 첫번째 파일을 불러온다
+    const file = e.target.files[0];
+
+    // 파일이 없으면 아무것도 하지 않는다
+    if (!file) {
+      return ;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      // fileReader.result로 url에 접근. 이미지를 가져온다
+      seetPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   };
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="picked food Image" fill/>}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -20,6 +44,7 @@ const ImagePicker = ({ label, name }) => {
           accept="image/png, image/jpeg"
           name={name}
           ref={ImageInputRef}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
