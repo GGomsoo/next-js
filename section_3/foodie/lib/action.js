d60643b7c41490f0b271e44d95080dee9802e613
Park Jong Hyuk <occultism22@naver.com>
2025-03-24 16:06:26 +0900

    126. 서버 사이드 입력 유효성 확인 추가 방법 shareMeal 에서 if 문을 통한 입력 데이터 양식 검증 함수 구현 관련 error 페이지 생성

--- section_3/foodie/lib/action.js
+++ section_3/foodie/lib/action.js
@@ -3,6 +3,11 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+// 빈 문자열인지 검증
+const isInvalidText = (text) => {
+  return !text || text.trim() === "";
+};
+
 // form과 연결, form에서 action을 사용하여 서버로 데이터를 전송
 export const shareMeal = async (formData) => {
   // form 데이터를 meal 객체로 변환
@@ -13,6 +18,19 @@ export const shareMeal = async (formData) => {
     image: formData.get("image"),
     creator: formData.get("name"),
     creator_email: formData.get("email"),
+  };
+
+  // 필수 입력칸 및 제출 양식 검증
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image || meal.image.size === 0
+  ) {
+    throw new Error("Invalid input");
   }
 
   await saveMeal(meal);
