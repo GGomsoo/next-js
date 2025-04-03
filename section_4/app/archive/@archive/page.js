1200777ce35ff192dd54bd2acc121f0c9672ebd0
Park Jong Hyuk <occultism22@naver.com>
2025-04-03 21:54:04 +0900

    142. 병렬 라우트 설정 및 사용 폴더명을 @로 시작한다 layout에서 children 을 받는 것 처럼, 폴더명으로 설정한 이름을 props로 받는다

---
+++ section_4/app/archive/@archive/page.js
@@ -0,0 +1,9 @@
+const ArchivePage = () => {
+  return (
+    <>
+      <h1>Archive Page</h1>
+    </>
+  );
+};
+
+export default ArchivePage;
---
+++ section_4/app/archive/@latest/page.js
@@ -0,0 +1,9 @@
+const LatestNewsPage = () => {
+  return (
+    <>
+      <h1>Latest News Page</h1>
+    </>
+  );
+};
+
+export default LatestNewsPage;
