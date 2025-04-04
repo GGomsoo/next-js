62045183ca53628e8a5ce46d7d296689f0d1c4da
Park Jong Hyuk <occultism22@naver.com>
2025-04-04 11:50:16 +0900

    146. Throwing (라우트 관련) 오류 잘못된 연도값을 url에 입력했을 경우 Error를 던져준다

--- section_4/app/archive/@archive/[[...filter]]/page.js
+++ section_4/app/archive/@archive/[[...filter]]/page.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const FilteredNewsPage = ({ params }) => {
   const filter = params.filter;
-  const selectedYear = filter?.[0] // 선택한 년도를 가져오기
-  const selectedMonth = filter?.[1] // 선택한 월을 가져오기
+  const selectedYear = filter?.[0]; // 선택한 년도를 가져오기
+  const selectedMonth = filter?.[1]; // 선택한 월을 가져오기
 
   let news;
   let links = getAvailableNewsYears();
@@ -16,21 +16,29 @@ const FilteredNewsPage = ({ params }) => {
 
     // 연도를 함수에 전달하면 월 식별자를 얻을 수 있음
     links = getAvailableNewsMonths(selectedYear);
-  }
+  };
 
   // 선택한 연도와 월이 있다면 해당 연도와 월의 뉴스를 가져오기
   // 뉴스를 불러온후엔 links를 빈 배열로 설정하여 연/월 헤더를 보여주지 않음
   if (selectedYear && selectedMonth) {
     news = getNewsForYearAndMonth(selectedYear, selectedMonth);
     links = []
-  }
+  };
 
   let newsContent = <p>No news found for the selected period.</p>
 
   // 뉴스가 있는 경우
   if (news && news.length > 0) {
     newsContent = <NewsList news={news} />;
-  }
+  };
+
+  // 선택한 연도는 있지만, 해당 연도에 뉴스가 없는 경우 에러를 출력
+  // 선택한 날짜의 type이 숫자가 아니기에 숫자로 변환해줘야 함
+  if (selectedYear && !getAvailableNewsYears().includes(+selectedYear) ||
+    selectedMonth && !getAvailableNewsMonths(selectedYear).includes(+selectedMonth)
+  ) {
+    throw new Error("Invalid filter.");
+  };
 
   return (
     <>
