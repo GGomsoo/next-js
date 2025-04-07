7ae0badc2b2b03bd4e78520034b64b00f753402a
Park Jong Hyuk <occultism22@naver.com>
2025-04-07 13:49:11 +0900

    162. 옵션 2: 서버 측 데이터 가져오기 useState, useEffect 필요X 클라이언트 컴포넌트 설정할 필요X 해당 페이지 async 설정 서버에서 랜더링 했기때문에 모든 뉴스내용 확인 가능

--- section_4/app/(content)/news/page.js
+++ section_4/app/(content)/news/page.js
@@ -1,48 +1,18 @@
-"use client";
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-const NewsPage = () => {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState();
+const NewsPage = async () => {
+  const res = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      const res = await fetch("http://localhost:8080/news");
-
-      if (!res.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      };
-
-      const news = await res.json();
-      setIsLoading(false);
-      setNews(news)
-    };
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!res.ok) {
+    throw new Error("Failed to fetch news");
   };
 
-  if (error) {
-    return <p>{error}</p>;
-  };
-
-  // 뉴스 존재여부 확인 후 제공
-  let newsContent
-  if (news) {
-    newsContent = <NewsList news={news} />
-  };
+  const news = await res.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 };
