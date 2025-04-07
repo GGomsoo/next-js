c33a55e2c6d6f8c681012c5fd8dd7931e96ee15f
Park Jong Hyuk <occultism22@naver.com>
2025-04-07 13:40:50 +0900

    161. 옵션 1: 클라이언트 측 데이터 가져오기 fetch, useEffect, useState 훅 활용 추가한 backend에 api요청하여 news 데이터 가져오기

--- section_4/app/(content)/news/page.js
+++ section_4/app/(content)/news/page.js
@@ -1,11 +1,48 @@
+"use client";
+
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 const NewsPage = () => {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const res = await fetch("http://localhost:8080/news");
+
+      if (!res.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      };
+
+      const news = await res.json();
+      setIsLoading(false);
+      setNews(news)
+    };
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  };
+
+  if (error) {
+    return <p>{error}</p>;
+  };
+
+  // 뉴스 존재여부 확인 후 제공
+  let newsContent
+  if (news) {
+    newsContent = <NewsList news={news} />
+  };
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS}/>
+      {newsContent}
     </>
   );
 };
