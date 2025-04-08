649ca5424ecc2e67e2500eeabd9eae122295a7db
Park Jong Hyuk <occultism22@naver.com>
2025-04-08 23:21:42 +0900

    187. 데이터 캐시 및 캐시 설정에 대한 이해 데이터 캐시에 대한 내용 fetch 함수의 옵션 중, cache 와 next를 활용 cache 옵션을 no-store로 설정하면 데이터를 캐싱하지 않고 항상 새 요청을 전송 next 설정의 revalidate 옵션 값은, 캐싱된 데이터를 사용할 시간(초)

--- section_7/app/messages/page.js
+++ section_7/app/messages/page.js
@@ -1,7 +1,18 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages');
+  const response = await fetch('http://localhost:8080/messages', {
+    // // 새 요청을 항상 전송
+    // // 그 요청의 응답 데이터를 사용하도록 강제
+    // // 데이터를 캐싱, 재사용 하지말라고 설정하는 옵션
+    // cache: "no-store"
+
+    // next 설정
+    // revalidate: 5, // 5초마다 재검증
+    next: {
+      revalidate: 5,
+    }
+  });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
