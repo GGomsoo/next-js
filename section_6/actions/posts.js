7f92e9704b89193cc8d93a7e6be75d4b61f1e524
Park Jong Hyuk <occultism22@naver.com>
2025-04-08 21:22:19 +0900

    176. 서버 액션을 별도의 파일에 저장 서버 액션의 로직이 복잡해지거나 주변 컴포넌트가 클라이언트 컴포넌트가 되어야 한다면 서버 액션에 대한 부분을 별도의 파일로 관리

---
+++ section_6/actions/posts.js
@@ -0,0 +1,43 @@
+"use server";
+
+import { redirect } from 'next/navigation';
+import { storePost } from '@/lib/posts';
+
+export async function createPost(prevState, formData) {
+  // 170
+  // 해당 함수를 서버 액션으로 만들기 위해서
+  // 함수 안에 "use server"를 추가해야 한다.
+  const title = formData.get('title');
+  const image = formData.get('image');
+  const content = formData.get('content');
+
+  let errors = [];
+
+  if (!title || !title.trim().length === 0) {
+    errors.push('Title is required');
+  };
+
+  if (!content || !content.trim().length === 0) {
+    errors.push('Content is required');
+  };
+
+  if (!image || image.size === 0) {
+    errors.push('Image is required');
+  };
+
+  if (errors.length > 0) {
+    // 174
+    // 에러가 있는 객체를 반환하여
+    // 클라이언트 측에서 해결 할 수 있음
+    return { errors };
+  }
+
+  await storePost({
+    imageUrl: '',
+    title,
+    content,
+    userId: 1
+  })
+
+  redirect("/feed");
+};
