6dd3ec2d1d13629e7b80cdd145b2af145d51cbf5
Park Jong Hyuk <occultism22@naver.com>
2025-04-09 16:33:58 +0900

    205. 동적 페이지 메타데이터 구성 generateMetadata 함수 활용 NextJS에서 해당 함수를 찾는데, 페이지가 로드 되면서 함수가 실행된다 이를 이용하여 페이지별로 다른 동적 메타데이터를 구성할 수 있음

--- section_8/app/feed/page.js
+++ section_8/app/feed/page.js
@@ -1,6 +1,17 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// 동적 메타데이터 생성
+// 함수 이름은 무조건 아래와 같게 해야함
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our (${numberOfPosts}) posts.`,
+    description: `Browse all our posts`,
+  };
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
