b914ac7d985e2bfb04deeb719a03740fea973ae6
Park Jong Hyuk <occultism22@naver.com>
2025-04-09 22:15:24 +0900

    216. 세션 및 세션 쿠키 구성하기 lucia를 통한 세션, 세션 쿠키 구성 NextJS의 cookies 함수를 이용, 응답에 포함된 쿠키에 접근

--- section_9/lib/auth.js
+++ section_9/lib/auth.js
@@ -1,6 +1,7 @@
 import { Lucia } from "lucia"
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite"
 import db from "./db";
+import { cookies } from "next/headers";
 
 const adapter = new BetterSqlite3Adapter(db, {
   user: "users",
@@ -15,3 +16,11 @@ const lucia = new Lucia(adapter, {
     }
   }
 });
+
+export const createAuthSession = async (userId) => {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  
+  // response에 포함된 쿠키에 접근할 수 있도록 도와주는 함수
+  cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+};
