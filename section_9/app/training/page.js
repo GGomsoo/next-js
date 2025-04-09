4921fc8bf3e54f4f32be2c1922b109916bf3b703
Park Jong Hyuk <occultism22@naver.com>
2025-04-09 22:33:17 +0900

    219. 인증되지 않은 액세스로부터 라우트 보호하기 로그인 상태에만 접속이 가능하도록 설정하려는 페이지에 사용자 검증 함수 기능을 추가 만약 user가 없다면 홈으로 되돌림

--- section_9/app/training/page.js
+++ section_9/app/training/page.js
@@ -1,6 +1,15 @@
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  const result = await verifyAuth();
+
+  // 로그인 하지않고 해당 페이지 접속시도시, 홈으로 리다이렉트
+  if (!result.user) {
+    return redirect("/");
+  };
+
   const trainingSessions = getTrainings();
 
   return (
