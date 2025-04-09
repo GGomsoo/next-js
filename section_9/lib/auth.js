183c859e47081084d26735ee6940cd8f2fdee61d
Park Jong Hyuk <occultism22@naver.com>
2025-04-09 22:29:35 +0900

    218. 활성 인증 세션 확인 인증된 사용자로부터 오는 요청인지 확인하는 작업 lucia를 통해 유효한 세션인지에 대한 검증을 진행

--- section_9/lib/auth.js
+++ section_9/lib/auth.js
@@ -20,7 +20,46 @@ const lucia = new Lucia(adapter, {
 export const createAuthSession = async (userId) => {
   const session = await lucia.createSession(userId, {});
   const sessionCookie = lucia.createSessionCookie(session.id);
-  
+
   // response에 포함된 쿠키에 접근할 수 있도록 도와주는 함수
   cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 };
+
+// 들어오는 요청이 인증된 사용자로부터 오는 요청인지 확인하는 함수
+export const verifyAuth = async () => {
+  const sessionCookie = cookies().get(lucia.sessionCookieName);
+  // 세션 쿠키가 없으면 유저랑 세션을 null로 설정
+  if (!sessionCookie) {
+    return {
+      user: null,
+      session: null,
+    };
+  };
+
+  const sessionId = sessionCookie.value;
+  // 세션 ID가 없다면 사용자 데이터 없이 객체를 반환
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  };
+
+  // 유효한 세션인지 검증
+  const result = await lucia.validateSession(sessionId);
+
+  try {
+    // 유효한 세션에 대해 세션 쿠키 설정
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    }
+
+    // 세션이 없을 경우 빈 세션 쿠키를 설정
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    }
+  } catch (err) {}
+  return result;
+}
