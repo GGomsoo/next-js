5084953acf0584d853dead46ee620db6974f6a51
Park Jong Hyuk <occultism22@naver.com>
2025-05-02 13:24:01 +0900

    244. 커스텀 404 페이지 추가하기 pages/404.js 파일 생성 next에서 404.js를 인식

---
+++ section_11/pages/404.js
@@ -0,0 +1,12 @@
+// 파일명을 404.js 로 설정하면
+// Next.js에서 자동으로 404 페이지로 인식합니다.
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
