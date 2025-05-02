53b2b01f363fd332decad4cb825627895e07b715
Park Jong Hyuk <occultism22@naver.com>
2025-05-02 13:22:00 +0900

    243. 프로그래밍 방식으로 내비게이팅하기 router.push 활용 경로를 직접 입력하는 방식도 가능 객체 생성하여 pathname, query 작성해도 가능

--- section_11/pages/clients/[id]/index.js
+++ section_11/pages/clients/[id]/index.js
@@ -4,9 +4,18 @@ const ClientsProjectPage = () => {
   const router = useRouter();
   const clientName = router.query.id;
   console.log(router.query);
+
+  const handleLoadProject = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "jonghyuk", clientprojectid: "1" },
+    });
+  };
+
   return (
     <div>
       <h1>{clientName}'s Project Page</h1>
+      <button onClick={handleLoadProject}>Load Project A</button>
     </div>
   );
 }
