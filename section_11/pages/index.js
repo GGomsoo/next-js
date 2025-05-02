7c182b77b635a0153ae6fe87e000afdeadeda115
Park Jong Hyuk <occultism22@naver.com>
2025-05-02 12:48:31 +0900

    240. Link 컴포넌트로 내비게이팅하기 a 태그로도 가능하지만 Link를 사용 앱 상태가 변하지 않는다

--- section_11/pages/index.js
+++ section_11/pages/index.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ul>
+        <li>
+          {/* replace 프로퍼티를 설정하면 
+          새로운 페이지를 또 띄우지 않고 현재 페이지를 새 페이지로 설정 가능 */}
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
